bdc848c310f06ad262f1349b67f92badbbb47c54
Arman Rajib <ahrajib6789@gmail.com>
2025-02-14 04:32:38 +0600

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "HypeNest",
+  description: "A modern e-commerce platform built with Next.js for HypeNest.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${manrope.className} antialiased`}>{children}</body>
+    </html>
+  );
+}

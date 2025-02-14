bd47abb70dbac1d6ab0b7d4b9add7b43c68109bf
Arman Rajib <ahrajib6789@gmail.com>
2025-02-14 07:01:49 +0600

    Add rootLayout and constants (from .env)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,16 @@
+import type { Metadata } from 'next'
+
 import { Button } from '@/components/ui/button'
+
+export const metadata: Metadata = {
+  title: 'Home'
+}
 
 export default function Home() {
   return (
-    <div className='ml-4 mt-2'>
+    <>
       <h1 className='h1-bold mb-4'>HypeNest</h1>
       <Button>Click me</Button>
-    </div>
+    </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from 'next'
 import { Manrope } from 'next/font/google'
+
 import './globals.css'
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants'
 
 const manrope = Manrope({
   subsets: ['latin']
 })
 
 export const metadata: Metadata = {
-  title: 'HypeNest',
-  description: 'A modern e-commerce platform built with Next.js for HypeNest.'
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
 }
 
 export default function RootLayout({

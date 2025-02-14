012bcc9754fd56bd33c172a6b7a37448c9ab499e
Arman Rajib <ahrajib6789@gmail.com>
2025-02-14 06:35:48 +0600

    Add shadcn ui and custom prettier settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from "next";
-import { Manrope } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Manrope } from 'next/font/google'
+import './globals.css'
 
 const manrope = Manrope({
-  subsets: ["latin"],
-});
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
-  title: "HypeNest",
-  description: "A modern e-commerce platform built with Next.js for HypeNest.",
-};
+  title: 'HypeNest',
+  description: 'A modern e-commerce platform built with Next.js for HypeNest.'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${manrope.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <div>HypeNest</div>;
+  return (
+    <div className='ml-4 mt-2'>
+      <h1 className='h1-bold mb-4'>HypeNest</h1>
+      <Button>Click me</Button>
+    </div>
+  )
 }

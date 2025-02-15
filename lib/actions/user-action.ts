3d3df854ef8c65232cd3060f95d7b14fc8d16d57
Arman Rajib <ahrajib6789@gmail.com>
2025-02-16 02:32:02 +0600

    Create sign-in and sign-out actions

---
+++ lib/actions/user-action.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import { signInFormSchema } from '@/lib/validators'
+import { signIn, signOut } from '@/auth'
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
+
+// Sign in the user with credientials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get('email') as string,
+      password: formData.get('password') as string
+    })
+
+    await signIn('click-up', user)
+    return { success: true, message: 'Signed in successfully' }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      // return { success: false, message: error.message }
+      throw error
+    }
+
+    return { success: false, message: 'Invalid email or password ' }
+  }
+}
+
+// Sign user out
+export async function signOutUser() {
+  await signOut()
+}

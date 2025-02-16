0351414c79dce0730c8cba4d80a891e3732c4cf3
Arman Rajib <ahrajib6789@gmail.com>
2025-02-17 03:54:21 +0600

    Implement signUpFormSchema using zod and create signUpUser action

--- lib/actions/user-action.ts
+++ lib/actions/user-action.ts
@@ -1,6 +1,10 @@
 'use server'
 
+import { hashSync } from 'bcrypt-ts-edge'
+import { prisma } from '@/db/prisma'
+
 import { signInFormSchema } from '@/lib/validators'
+import { signUpFormSchema } from '@/lib/validators'
 import { signIn, signOut } from '@/auth'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 
@@ -30,4 +34,40 @@ export async function signInWithCredentials(
 // Sign user out
 export async function signOutUser() {
   await signOut()
+}
+
+// Sign up a user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name') as string,
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+      confirmPassword: formData.get('confirmPassword') as string
+    })
+
+    const plainPassword = user.password
+    user.password = hashSync(user.password, 12)
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password
+      }
+    })
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword
+    })
+
+    return { success: true, message: 'Signed up successfully' }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
+    }
+
+    return { success: false, message: 'User is not registered!' }
+  }
 }
